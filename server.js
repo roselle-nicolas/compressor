30b1b759d41f1a033a9ac8494b9a3161bc88da9e
DevonTheFloor <devonthefloor@free.fr>
2021-03-15 07:42:56 +0100

    Fix non fetch de pull

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = require('./app')
 const nport = 3333
 
 
-app.set('port',process.env.PORT || nport);
+app.set('port', process.env.PORT || nport)
 const server = http.createServer(app)
 console.log(`Connecté au port ${nport}`)
 
-server.listen(process.env.PORT || nport)
+server.listen( process.env.PORT || nport)
