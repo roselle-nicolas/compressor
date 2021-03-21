25f6f90027ed2e377ed07c2e43c9152c2cef68e4
DevonTheFloor <devonthefloor@free.fr>
2021-03-21 08:17:25 +0100

    Ajout branch lib2 nouvelle lib de compression

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const compress_images = require('compress-images')
+const INPUT_path = './img/ter1.jpg'
+const OUTPUT_path = './comp-img/comp-'
 
 // route
 
@@ -13,8 +15,24 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(bodyParser.json())
+app.use(express.json())
 
+app.use('/test2/', () => {
+  console.log('in da funcchionne')
+  compress_images(INPUT_path, OUTPUT_path, { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+    { png: { engine: 'pngquant', command: ['--quality=20-50', '-o'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
+    function logg (error, completed, statistic) {
+      console.log('in da CallBack')
+      console.log('-------------')
+      console.log(error)
+      console.log(completed)
+      console.log(statistic)
+      console.log('-------------')
+    })
+})
 //route
 //auth
 
