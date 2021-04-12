9768f19b2cafab214d16dda2cbd37f59b9f92453
roselle nicolas <roselle.nicolas.pro@gmail.com>
2021-04-12 16:27:07 +0200

    changement du dossier static

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 //route
 app.use('/api/onepic', formatRoutes)
-app.use('/assets', express.static(path.join(__dirname, 'comp-img/comp-temp')))
+app.use('/assets', express.static(path.join(__dirname, 'comp-img')))
 
 //auth
 
