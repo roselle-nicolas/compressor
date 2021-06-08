28970fa741f09beaf9be950b76ad860408a0b122
nicolas roselle <60002258+roselle-nicolas@users.noreply.github.com>
2021-06-08 09:12:14 +0200

    ajout /api/auth/get-user/:userId

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,13 +37,12 @@ exports.singup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-    console.log('logo enter', req.body.username, req.body.password);
     //fonction interne de comparaison de mot de passe crypté
     const bcriptCompare = (user) => {
         bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            return res.status(401).json({ message: 'Mot de passe incorrect !' });
           }
           console.log('admin : user', user);
           res.status(200).json({
@@ -67,7 +66,7 @@ exports.login = (req, res, next) => {
         User.findOne({ email: req.body.userName})
             .then(user => {
                     if(!user) {
-                        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                        return res.status(401).json({ message: 'Utilisateur non trouvé !' });
                     }else {
                         bcriptCompare(user);
                     }
@@ -79,4 +78,22 @@ exports.login = (req, res, next) => {
       }
     })
     .catch(error => res.status(500).json(error));
+}
+
+exports.getUser = (req, res, next) => {
+  const userId = req.params.userId;
+  User.findOne({ _id: userId})
+            .then(user => {
+                    if(!user) {
+                        return res.status(401).json({ message: 'Utilisateur non trouvé !' });
+                    }else {
+                      res.status(200).json({
+                        userId: user._id,
+                        pseudo: user.pseudo,
+                        email: user.email,
+                      });
+                    }
+                    
+            })
+            .catch(error => res.status(500).json(error));
 }
