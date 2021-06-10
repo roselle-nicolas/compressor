2db6ea122482e4ceb316ce30636547823ccbbad4
nicolas roselle <60002258+roselle-nicolas@users.noreply.github.com>
2021-06-10 09:14:34 +0200

    revue de code: suppression de log

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,97 +3,93 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const ENV = process.env;
 
-exports.singup = (req, res, next) => {
-    console.log('register enter ok');
-    console.log(req.body);
+exports.singup = (req, res) => {
     bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-            pseudo : req.body.pseudo,
-            email: req.body.email,
-            password: hash
-        });
-        console.log('type password', typeof user.password);
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => {
-            let messageError= "";
-            // console.log("error singup",error.errors);
-            if (error.errors.email) {
-              messageError += "Cette email existe déjà."
-            }
-            if (error.errors.pseudo) {
-              messageError += " Ce peudo existe déjà."
-            }
-            if (messageError !== "") {
-              res.status(400).json({ message : messageError });
-            } else {
-              res.status(400).json({ message : error });
-            }
-            res.status(500).json({error});
-          });
-      })
-      .catch(error => res.status(500).json({ message : error }));
+          .then(hash => {
+            const user = new User({
+                pseudo : req.body.pseudo,
+                email: req.body.email,
+                password: hash
+            });
+
+            user.save()
+                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                .catch(error => {
+                  let messageError= "";
+                  if (error.errors.email) {
+                    messageError += "Cette email existe déjà."
+                  }
+                  if (error.errors.pseudo) {
+                    messageError += " Ce peudo existe déjà."
+                  }
+                  if (messageError !== "") {
+                    res.status(400).json({ message : messageError });
+                  } else {
+                    res.status(400).json({ message : error });
+                  }
+                  res.status(500).json({error});
+                });
+          })
+          .catch(error => res.status(500).json({ message : error }));
 }
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
     //fonction interne de comparaison de mot de passe crypté
     const bcriptCompare = (user) => {
         bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ message: 'Mot de passe incorrect !' });
-          }
-          console.log('admin : user', user);
-          res.status(200).json({
-            userId: user._id,
-            pseudo: user.pseudo,
-            email: user.email,
-            token: jwt.sign(
-              { userId: user._id },
-              ENV.RANDOM_TOKEN_SECRET,
-              { expiresIn: '1h' }
-            )
-          });
-        })
-        .catch(error => res.status(500).json(error));
+              .then(valid => {
+                if (!valid) {
+                  return res.status(401).json({ message: 'Mot de passe incorrect !' });
+                }
+                console.log('admin : user', user);
+                res.status(200).json({
+                  userId: user._id,
+                  pseudo: user.pseudo,
+                  email: user.email,
+                  token: jwt.sign(
+                    { userId: user._id },
+                    ENV.RANDOM_TOKEN_SECRET,
+                    { expiresIn: '1h' }
+                  )
+                });
+              })
+              .catch(error => res.status(500).json(error));
       }
     
     User.findOne({ pseudo: req.body.username})
-    .then(user => {
-        console.log("user",user);
-      if (!user) {
-        User.findOne({ email: req.body.userName})
-            .then(user => {
+        .then(user => {
+          if (!user) {
+            User.findOne({ email: req.body.userName})
+                .then(user => {
                     if(!user) {
                         return res.status(401).json({ message: 'Utilisateur non trouvé !' });
                     }else {
                         bcriptCompare(user);
                     }
-                    
-            })
-            .catch(error => res.status(500).json(error));
-      }else {
-          bcriptCompare(user);
-      }
-    })
-    .catch(error => res.status(500).json(error));
+                        
+                })
+                .catch(error => res.status(500).json(error));
+          }else {
+              bcriptCompare(user);
+          }
+        })
+        .catch(error => res.status(500).json(error));
 }
 
-exports.getUser = (req, res, next) => {
+exports.getUser = (req, res) => {
   const userId = req.params.userId;
   User.findOne({ _id: userId})
-            .then(user => {
-                    if(!user) {
-                        return res.status(401).json({ message: 'Utilisateur non trouvé !' });
-                    }else {
-                      res.status(200).json({
-                        userId: user._id,
-                        pseudo: user.pseudo,
-                        email: user.email,
-                      });
-                    }
-                    
-            })
-            .catch(error => res.status(500).json(error));
+      .then(user => {
+              if(!user) {
+                  return res.status(401).json({ message: 'Utilisateur non trouvé !' });
+              }else {
+                res.status(200).json({
+                  userId: user._id,
+                  pseudo: user.pseudo,
+                  email: user.email,
+                });
+              }
+              
+      })
+      .catch(error => res.status(500).json(error));
 }
