6b6d5f528ef752cd581f3dbc7a0e6e9e142b5076
nicolas roselle <60002258+roselle-nicolas@users.noreply.github.com>
2021-06-12 18:07:38 +0200

    ajout protection auth

--- routes/formatSocket.js
+++ routes/formatSocket.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const multer = require("../middlewares/multer-config");
 // const formatCtrl = require("../controllers/format")
 const formatSocketCtrl = require("../controllers/formatSocket");
+const auth = require("../middlewares/auth");
  
 
-router.post("/jpg/", multer, formatSocketCtrl.jpgComp);
-router.post("/png/",multer, formatSocketCtrl.pngComp);
-router.post("/gif/",multer, formatSocketCtrl.gifComp);
-router.post("/svg",multer, formatSocketCtrl.svgComp);
+router.post("/jpg/",auth, multer, formatSocketCtrl.jpgComp);
+router.post("/png/", auth, multer, formatSocketCtrl.pngComp);
+router.post("/gif/",auth, multer, formatSocketCtrl.gifComp);
+router.post("/svg",auth, multer, formatSocketCtrl.svgComp);
 
 module.exports = router;
